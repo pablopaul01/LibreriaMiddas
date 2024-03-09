f439716051ef1c1cc8ca698e636fe893452e6a83
pablopaul01 <juanpablosalomon@gmail.com>
2024-03-09 18:42:17 -0300

    se crea un helper para obtener los datos de los libros

---
+++ Front/src/helpers/dataBooks.js
@@ -0,0 +1,10 @@
+import { axiosInstance } from '../config/axiosInstance'
+
+export const getBooks = async (setBooks) => {
+    try {
+      const response = await axiosInstance.get('/books')
+      setBooks(response.data.books)
+    } catch (error) {
+      console.log("el error", error.response.data.mensaje)
+    }
+}
--- Front/src/pages/Books.jsx
+++ Front/src/pages/Books.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import { getBooks } from '../helpers/dataBooks'
 
 const Books = () => {
+    const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    getBooks(setBooks)
+  }, [])
+
+  console.log("books", books)
+
   return (
-    <div>Libreria Middas</div>
+    <div>
+        Libreria Middas
+        
+    </div>
   )
 }
 
