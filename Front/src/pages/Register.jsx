a03f451851ee06c5fc8c99ad6c71b5da606d9f0b
pablopaul01 <juanpablosalomon@gmail.com>
2024-03-12 11:28:43 -0300

    se agrega el formulario para el registro de usuario

--- Front/src/pages/Register.jsx
+++ Front/src/pages/Register.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CardUser from '../components/CardUser'
+import RegisterForm from '../components/RegisterForm'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='h-[100vh] bg-slate-100 flex justify-center items-center'>
+        <CardUser title={"Registrarse"} subTitle={"Completa los datos para crear tu cuenta"}>
+          <RegisterForm />
+        </CardUser>
+    </div>
   )
 }
 
